0451719ddbe3a17805068c736085cd67603c103f
Rishil-patel <Rishilpatel2864@gmail.com>
2026-06-17 14:58:10 +0530

    clean db.js file removing comments

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,25 +8,12 @@ const pool = new Pool({
   },
 });
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: "postgres",
-//     port: process.env.DB_PORT,
-//   },
-// );
-
 const connectDB = async () => {
   try {
     const client = await pool.connect();
     console.log("✅ Neo PostgreSQL Connected");
     client.release();
   } catch (error) {
-    console.log("hello");
-
     console.log("❌ Database Error:-->", error.message);
   }
 };
